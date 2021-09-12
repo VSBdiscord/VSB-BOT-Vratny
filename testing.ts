680d068947ec3ee1c1c4cf810c8d79f77dc1d00d
Lukáš Majoros <34129340+KasheK420@users.noreply.github.com>
2021-09-12 12:13:50 +0200

    Update testing.ts
    Changed testing email, will be send to DL.

--- testing.ts
+++ testing.ts
@@ -1,7 +1,7 @@
 import * as Mail from "./libs/Mail";
 
 console.log("Sending mail.");
-Mail.Send("mil0068@vsb.cz", "Test", "Test").then(value => {
+Mail.Send("discord@vsb.cz", "Test", "Test").then(value => {
     console.log("SENT!");
     console.log(value);
 }).catch(reason => {
